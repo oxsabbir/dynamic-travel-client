344f51027f7b1fc122555a0cdab179057e72e71f
Sabbir Hossain <sabbir2always@gmail.com>
2024-11-19 19:11:32 +0600

    add download and close button for image slider

--- app/ui/ImageSlider.jsx
+++ app/ui/ImageSlider.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -8,35 +9,52 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 import "./imageSlider.css";
+import { IconButton, Typography } from "@material-tailwind/react";
+import { HiOutlineDownload, HiOutlineX } from "react-icons/hi";
 
 // import required modules
 import { Pagination, Navigation } from "swiper/modules";
 
-export default function ImageCarousel({ images }) {
+export default function ImageCarousel({ handleOpen, images }) {
+  const [activeImageIndex, setActiveImageIndex] = useState(null);
   return (
     <>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className=" w-full !h-full"
-      >
-        {images &&
-          images?.map((item, i) => (
-            <SwiperSlide key={i}>
-              <img
-                src={item}
-                alt={`tour_image_${i}`}
-                className="!h-full w-full"
-              />
-            </SwiperSlide>
-          ))}
-      </Swiper>
+      <div className="rounded-lg shadow-md w-full h-[95%] relative p-3 bg-offWhite">
+        <div className=" flex justify-between px-3 items-center mb-2 ">
+          <Typography className=" tracking-wider text-gray-900">
+            Feature Image
+          </Typography>
+          <div className=" flex gap-2 items-center">
+            <IconButton onClick={handleOpen} variant="gradient" size="sm">
+              <HiOutlineX className=" w-6 h-6" />
+            </IconButton>
+            <a href={activeImageIndex >= 0 && images[activeImageIndex]}>
+              <IconButton variant="gradient" size="sm">
+                <HiOutlineDownload className=" w-6 h-6" />
+              </IconButton>
+            </a>
+          </div>
+        </div>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={0}
+          loop={true}
+          onSlideChange={(value) => setActiveImageIndex(value?.activeIndex)}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className=" rounded-lg shadow-lg "
+        >
+          {images &&
+            images?.map((item, i) => (
+              <SwiperSlide key={i}>
+                <img src={item} alt={`tour_image_${i}`} className=" w-full  " />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
     </>
   );
 }
