d9a31b6892ad13a9cf142344a647d91aad1faa5a
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-04 10:26:41 +0600

    fix layout for the profile page

--- app/(routes)/profile/[userName]/page.jsx
+++ app/(routes)/profile/[userName]/page.jsx
@@ -14,78 +14,80 @@ export default async function page({ params }) {
 
   return (
     <>
-      <div
-        style={{
-          backgroundImage: `url("${profileGradient.src}")`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
-        className="h-[240px]"
-      >
-        <Nav />
-      </div>
-      <Container>
-        <div>
-          <div className="lg:-translate-y-[18%] -translate-y-[15%]  ">
-            <div className="w-full flex flex-col  pb-4 lg:flex-row lg:justify-start gap-2 items-center justify-center">
-              <Image
-                src={userData?.profileImage}
-                alt="profileImage"
-                width={265}
-                height={265}
-                quality={90}
-                className="lg:w-[265px] lg:h-[265px] h-[240px] w-[240px] border-[10px] border-[#7575754c] rounded-[50px]"
-              />
-              <div className=" self-end p-2 lg:p-5 gap-4 flex flex-col lg:flex-row items-center justify-between w-full">
-                <div className=" text-center lg:text-left">
-                  <Typography
-                    variant="lead"
-                    className=" text-textBlack tracking-wider font-bold text-3xl mb-4"
-                  >
-                    {userData?.fullName}
-                  </Typography>
-                  <Typography
-                    variant="paragraph"
-                    className="tracking-wide text-offGray  max-w-[300px]"
-                  >
-                    Web developer and Product designer based on San Antonio
-                  </Typography>
-                </div>
-                <div className="flex gap-3 items-center lg:p-2 pb-0">
-                  <div className=" p-2">
-                    <Typography className=" text-offBlack font-medium tracking-wide text-base">
-                      Completed
+      <div>
+        <div className=" relative">
+          <div
+            style={{
+              backgroundImage: `url("${profileGradient.src}")`,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            className=" h-[250px] bg-green-300 "
+          >
+            <Nav />
+          </div>
+          <div className=" absolute top-[55%] left-[50%] -translate-x-[50%] w-full">
+            <Container>
+              <div className="w-full flex flex-col  pb-4 lg:flex-row lg:justify-start gap-2 items-center justify-center">
+                <Image
+                  src={userData?.profileImage}
+                  alt="profileImage"
+                  width={265}
+                  height={265}
+                  quality={90}
+                  className="lg:w-[265px] lg:h-[265px] h-[240px] w-[240px] border-[8px] border-[#b6b6b64c] rounded-[45px]"
+                />
+                <div className=" self-end p-2 lg:p-5 gap-4 flex flex-col lg:flex-row items-center justify-between w-full">
+                  <div className=" text-center lg:text-left">
+                    <Typography
+                      variant="lead"
+                      className=" text-textBlack tracking-wider font-bold text-3xl mb-4"
+                    >
+                      {userData?.fullName}
                     </Typography>
-                    <Typography className=" font-extrabold text-4xl text-offBlack mt-2 tracking-wider">
-                      56
+                    <Typography
+                      variant="paragraph"
+                      className="tracking-wide text-offGray  max-w-[300px]"
+                    >
+                      Web developer and Product designer based on San Antonio
                     </Typography>
                   </div>
-                  <div className=" p-2">
-                    <Typography className=" text-offBlack font-medium tracking-wide text-base">
-                      Upcoming
-                    </Typography>
-                    <Typography className=" font-extrabold text-4xl text-offBlack mt-2 tracking-wider">
-                      4
-                    </Typography>
-                  </div>
-                  <div className=" p-2">
-                    <Typography className=" text-offBlack font-medium tracking-wide text-base">
-                      To Review
-                    </Typography>
-                    <Typography className=" font-extrabold text-4xl text-offBlack mt-2 tracking-wider">
-                      14
-                    </Typography>
+                  <div className="flex gap-3 items-center lg:p-2 pb-0">
+                    <div className=" p-2">
+                      <Typography className=" text-offBlack font-medium tracking-wide text-base">
+                        Completed
+                      </Typography>
+                      <Typography className=" font-extrabold text-4xl text-offBlack mt-2 tracking-wider">
+                        56
+                      </Typography>
+                    </div>
+                    <div className=" p-2">
+                      <Typography className=" text-offBlack font-medium tracking-wide text-base">
+                        Upcoming
+                      </Typography>
+                      <Typography className=" font-extrabold text-4xl text-offBlack mt-2 tracking-wider">
+                        4
+                      </Typography>
+                    </div>
+                    <div className=" p-2">
+                      <Typography className=" text-offBlack font-medium tracking-wide text-base">
+                        To Review
+                      </Typography>
+                      <Typography className=" font-extrabold text-4xl text-offBlack mt-2 tracking-wider">
+                        14
+                      </Typography>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="min-h-[300px] ">
-              <ProfileTour userName={userName} />
-            </div>
+              <div className="min-h-[300px] ">
+                <ProfileTour userName={userName} />
+              </div>
+            </Container>
           </div>
         </div>
-      </Container>
+      </div>
     </>
   );
 }
