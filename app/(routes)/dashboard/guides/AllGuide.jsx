b1e212f29d5c2d38110042ee5c89bd3e5a9f8eef
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-14 07:10:45 +0600

    fix responsiveness for tour-guide card

--- app/(routes)/dashboard/guides/AllGuide.jsx
+++ app/(routes)/dashboard/guides/AllGuide.jsx
@@ -157,7 +157,7 @@ export default function AllGuide() {
             </Typography>
           </div>
         )}
-        <div className=" grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
           {guides?.length > 0 &&
             !loading &&
             guides?.map((item) => (
