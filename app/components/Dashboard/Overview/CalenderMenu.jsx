4349250be19cfd35806dc24ddc91e0d0e1e71490
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-17 10:52:48 +0600

    add function for booked date in the calender for future uses

--- app/components/Dashboard/Overview/CalenderMenu.jsx
+++ app/components/Dashboard/Overview/CalenderMenu.jsx
@@ -3,10 +3,32 @@ import "react-calendar/dist/Calendar.css";
 import "@/app/components/Dashboard/Overview/Calender.css";
 
 export default function CalenderMenu() {
+  const bookedDates = ["2024-03-10", "2024-03-15", "2024-03-27", "2024-03-30"]; // Specify the dates where events are booked
+
+  const tileContent = ({ date }) => {
+    const dateString = date.toISOString().split("T")[0];
+    const isBooked = bookedDates.includes(dateString);
+
+    return (
+      <div>
+        {isBooked && (
+          <div
+            className=" bg-pink-300"
+            style={{
+              width: "7px",
+              height: "7px",
+              borderRadius: "50%",
+              margin: "0 auto",
+            }}
+          />
+        )}
+      </div>
+    );
+  };
   return (
     <>
       <div className="">
-        <Calendar showNavigation={false} />
+        <Calendar showNavigation={false} tileContent={tileContent} />
       </div>
     </>
   );
