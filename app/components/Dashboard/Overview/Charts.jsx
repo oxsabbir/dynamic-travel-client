ad877bf46ffe974c13f82f00a889c6616f19f4a2
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-18 09:57:29 +0600

    fix the styler for other charts using grid layout

--- app/components/Dashboard/Overview/Charts.jsx
+++ app/components/Dashboard/Overview/Charts.jsx
@@ -6,10 +6,12 @@ import ChartBar from "./ChartBar";
 export default function Charts() {
   return (
     <>
-      <div className=" ">
-        <Card className="">
-          <ChartBar />
-        </Card>
+      <div className="  pb-8 pt-2 h-full 2xl:h-[840px]  grid  grid-cols-1 lg:grid-cols-8 2xl:grid-cols-4  grid-rows-1 gap-4">
+        <div className=" ">
+          <Card className="">
+            <ChartBar />
+          </Card>
+        </div>
       </div>
     </>
   );
