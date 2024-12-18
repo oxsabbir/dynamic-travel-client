ce9cc0decb9a88e7040c264737d4e9c927ebac82
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-18 09:24:21 +0600

    import required barcharts and defined layout

--- app/components/Dashboard/Overview/ChartBar.jsx
+++ app/components/Dashboard/Overview/ChartBar.jsx
@@ -1,10 +1,24 @@
 import { Button, Typography } from "@material-tailwind/react";
 import React from "react";
+import {
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import { BarChart } from "recharts";
+import { ChevronDown, ChevronUp } from "lucide-react";
 
 export default function ChartBar() {
   return (
     <>
-      <div className=" "></div>
+      <div className=" flex justify-between flex-col p-5">
+        <div className=" flex justify-between items-center">
+          <h4 className=" text-sm">New Audiences</h4>
+        </div>
+      </div>
     </>
   );
 }
