ed12701691cd7ad36b5921ebb61c5f5236011cf4
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-18 09:15:18 +0600

    add barchart body

---
+++ app/components/Dashboard/Overview/ChartBar.jsx
@@ -0,0 +1,10 @@
+import { Button, Typography } from "@material-tailwind/react";
+import React from "react";
+
+export default function ChartBar() {
+  return (
+    <>
+      <div className=" "></div>
+    </>
+  );
+}
