c13b9ae9d38bb3b08cf90661ee39ee1a10f0c2fd
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-19 07:28:26 +0600

    add latest booking template

--- app/components/Dashboard/Overview/Charts.jsx
+++ app/components/Dashboard/Overview/Charts.jsx
@@ -2,16 +2,21 @@
 import React from "react";
 import { Card } from "@material-tailwind/react";
 import ChartBar from "./ChartBar";
+import LatestBookings from "./LatestBookings";
 
 export default function Charts() {
   return (
     <>
       <div className="  pb-8 pt-2 h-full 2xl:h-[840px]  grid  grid-cols-1 lg:grid-cols-8 2xl:grid-cols-4  grid-rows-1 gap-4">
-        <div className=" ">
+        <div className=" 2xl:col-span-2 lg:row-span-1 col-span-4 lg:col-span-5 2xl:row-span-3">
           <Card className="">
             <ChartBar />
           </Card>
         </div>
+
+        <Card className=" row-span-2 2xl:row-span-4 lg:col-span-3  overflow-auto  2xl:col-span-1 col-span-4">
+          <LatestBookings />
+        </Card>
       </div>
     </>
   );
