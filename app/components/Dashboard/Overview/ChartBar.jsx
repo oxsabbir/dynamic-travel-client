3561acbd25003f76ba5e5cf258b55d4b7a987e09
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-26 08:16:23 +0600

    imported required module and create custom yaxis tick

--- app/components/Dashboard/Overview/ChartBar.jsx
+++ app/components/Dashboard/Overview/ChartBar.jsx
@@ -1,5 +1,36 @@
 "use client";
 import { Card } from "@material-tailwind/react";
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const CustomizedYAxisTick = (props) => {
+  const { x, y, payload } = props;
+  const value = payload.value / 1000; // convert to 'k'
+
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text
+        x={0}
+        y={0}
+        dy={0}
+        textAnchor="end"
+        fontSize={13}
+        fontWeight={500}
+        fill="#666"
+      >
+        {value}k
+      </text>
+    </g>
+  );
+};
+
 export default function AquisitionChart() {
   return (
     <Card className=" p-4 flex justify-center ">
