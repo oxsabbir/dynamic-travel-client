1816b8bec1b7c98b63a8a884b94f0f4ef2f238da
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-27 09:11:20 +0600

    add skeleton to sales card

--- app/components/Dashboard/Overview/Sells.jsx
+++ app/components/Dashboard/Overview/Sells.jsx
@@ -10,18 +10,21 @@ import {
 } from "lucide-react";
 import CalenderMenu from "./CalenderMenu";
 import { getSellsStats } from "@/app/libs/overviewApi";
+import { SalesSkeleton } from "@/app/ui/skeleton/SalesBar";
 
 export default function Sells({ filterby }) {
   const [sellsStats, setSellsStats] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
       try {
         const sellsData = await getSellsStats(filterby);
-        console.log(sellsData);
         setSellsStats(sellsData);
+        setLoading(false);
       } catch (error) {
         console.log(error);
+        setLoading(false);
         throw error;
       }
     };
@@ -55,47 +58,63 @@ export default function Sells({ filterby }) {
   return (
     <>
       <div className=" grid grid-cols-1 items-center md:grid-cols-2 xl:grid-cols-4 gap-6  py-4">
-        {data.map((item) => (
-          <Card key={item.title}>
-            <div
-              key={item.title}
-              className=" flex justify-between py-3 px-5 items-center border-b-[2px] border-[#CCCCCC]"
-            >
-              <div className=" flex gap-3 items-center font-bold">
-                <item.icon className=" w-7 h-7" />
-                <Typography className=" font-semibold">{item.title}</Typography>
+        {loading && (
+          <>
+            {data.map((item, i) => (
+              <Card key={i}>
+                <div className=" flex justify-between py-3 px-5 items-center border-b-[2px] border-[#CCCCCC]">
+                  <SalesSkeleton />
+                </div>
+              </Card>
+            ))}
+          </>
+        )}
+
+        {!loading &&
+          data[0]?.amount &&
+          data.map((item) => (
+            <Card key={item.title}>
+              <div
+                key={item.title}
+                className=" flex justify-between py-3 px-5 items-center border-b-[2px] border-[#CCCCCC]"
+              >
+                <div className=" flex gap-3 items-center font-bold">
+                  <item.icon className=" w-7 h-7" />
+                  <Typography className=" font-semibold">
+                    {item.title}
+                  </Typography>
+                </div>
+
+                <CircleEllipsis className=" float-right" />
               </div>
 
-              <CircleEllipsis className=" float-right" />
-            </div>
-
-            <div className="p-6 px-7  flex flex-col justify-center">
-              <Typography variant="h4" className=" font-semibold pb-4">
-                {item.title !== "Total Bookings" ? "$" : ""}
-                {item.title !== "Total Bookings" ? item.amount : item.amount}
-              </Typography>
-              <div className=" flex items-center">
-                <span>
+              <div className="p-6 px-7  flex flex-col justify-center">
+                <Typography variant="h4" className=" font-semibold pb-4">
+                  {item.title !== "Total Bookings" ? "$" : ""}
+                  {item.title !== "Total Bookings" ? item.amount : item.amount}
+                </Typography>
+                <div className=" flex items-center">
+                  <span>
+                    <Typography
+                      variant="small"
+                      className={`font-medium ${
+                        item.increase ? "positive" : "negative"
+                      } px-5 mr-3 rounded-2xl`}
+                    >
+                      {item.increase ? "+" : "-"} {item.change}
+                      {!item.increase && "100 "}
+                    </Typography>
+                  </span>
                   <Typography
                     variant="small"
-                    className={`font-medium ${
-                      item.increase ? "positive" : "negative"
-                    } px-5 mr-3 rounded-2xl`}
+                    className=" text-sm  font-normal text-dashboard_text"
                   >
-                    {item.increase ? "+" : "-"} {item.change}
-                    {!item.increase && "100 "}
+                    From last period
                   </Typography>
-                </span>
-                <Typography
-                  variant="small"
-                  className=" text-sm  font-normal text-dashboard_text"
-                >
-                  From last period
-                </Typography>
+                </div>
               </div>
-            </div>
-          </Card>
-        ))}
+            </Card>
+          ))}
 
         <div className=" grid-cols-1 grid-rows-1">
           <CalenderMenu />
---
+++ app/ui/skeleton/SalesBar.jsx
@@ -0,0 +1,43 @@
+import { Typography } from "@material-tailwind/react";
+
+export function SalesSkeleton() {
+  return (
+    <div className="max-w-full animate-pulse py-2">
+      <Typography
+        as="div"
+        variant="h1"
+        className="mb-4 h-5 w-56 rounded-full bg-gray-300"
+      >
+        &nbsp;
+      </Typography>
+      <Typography
+        as="div"
+        variant="paragraph"
+        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
+      >
+        &nbsp;
+      </Typography>
+      <Typography
+        as="div"
+        variant="paragraph"
+        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
+      >
+        &nbsp;
+      </Typography>
+      <Typography
+        as="div"
+        variant="paragraph"
+        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
+      >
+        &nbsp;
+      </Typography>
+      <Typography
+        as="div"
+        variant="paragraph"
+        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
+      >
+        &nbsp;
+      </Typography>
+    </div>
+  );
+}
