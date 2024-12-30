4178ddd7df94438139da202c9623c42f33dd5c74
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-30 09:36:13 +0600

    setup dummy data for userJoin chart

--- app/components/Dashboard/Overview/UserJoinStats.jsx
+++ app/components/Dashboard/Overview/UserJoinStats.jsx
@@ -1,7 +1,66 @@
 "use client";
 import { Card } from "@material-tailwind/react";
+import {
+  XAxis,
+  YAxis,
+  LineChart,
+  Tooltip,
+  Line,
+  ResponsiveContainer,
+} from "recharts";
 
 export default function UserJoinStats() {
+  const carData = [
+    {
+      month: "JAN",
+      Traffic: 124236,
+    },
+    {
+      month: "FEB",
+      Traffic: 323564,
+    },
+    {
+      month: "MAR",
+      Traffic: 453452,
+    },
+    {
+      month: "APR",
+      Traffic: 1945343,
+    },
+    {
+      month: "MAY",
+      Traffic: 644212,
+    },
+    {
+      month: "JUN",
+      Traffic: 1766412,
+    },
+    {
+      month: "JUL",
+      Traffic: 342355,
+    },
+    {
+      month: "AUG",
+      Traffic: 523543,
+    },
+    {
+      month: "SEP",
+      Traffic: 945344,
+    },
+    {
+      month: "OCT",
+      Traffic: 342355,
+    },
+    {
+      month: "NOV",
+      Traffic: 145676,
+    },
+    {
+      month: "DEC",
+      Traffic: 14534,
+    },
+  ];
+
   return (
     <>
       <Card className=" p-4 flex justify-center h-full ">
