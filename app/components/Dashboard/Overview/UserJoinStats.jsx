32a2d544489537c56891536ae50bb3f18c77b676
Sabbir Hossain <sabbir2always@gmail.com>
2024-12-30 09:44:52 +0600

    render actual chart only with dummy data

--- app/components/Dashboard/Overview/UserJoinStats.jsx
+++ app/components/Dashboard/Overview/UserJoinStats.jsx
@@ -61,6 +61,20 @@ export default function UserJoinStats() {
     },
   ];
 
+  const CustomizedYAxisTick = (props) => {
+    const { x, y, payload } = props;
+    const value =
+      payload.value < 1000 ? payload.value : `${payload.value / 1000}k`; // convert to 'k'
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text textAnchor="end" fontSize={13} fill="#666">
+          {value}
+        </text>
+      </g>
+    );
+  };
+
   return (
     <>
       <Card className=" p-4 flex justify-center h-full ">
@@ -79,6 +93,49 @@ export default function UserJoinStats() {
               Bars
             </option>
           </select>
+        </div>
+        <div className=" h-full min-h-[230px]  p-4">
+          <ResponsiveContainer>
+            <LineChart
+              data={carData}
+              margin={{
+                top: 15,
+                right: 18,
+                left: -10,
+                bottom: 0,
+              }}
+            >
+              <XAxis
+                dataKey="month"
+                fontSize={12}
+                fontWeight={500}
+                tickLine={false}
+                axisLine={false}
+              />
+              <YAxis
+                tickLine={false}
+                axisLine={false}
+                tick={<CustomizedYAxisTick />}
+              />
+
+              <defs>
+                <linearGradient id="linear" x1="1" y1="0" x2="0" y2="0">
+                  <stop offset="10%" stopColor="#1B59F8" stopOpacity={1} />
+                  <stop offset="100%" stopColor="#4673E7" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              <Tooltip />
+              <Line
+                type="monotone"
+                dataKey="Traffic"
+                // stroke="#526EF3"
+                stroke="url(#linear)"
+                strokeWidth={9}
+                dot={false}
+                strokeLinecap="round"
+              />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
       </Card>
     </>
