f104e7e128807b8de0a25a0d6bd52516f70163ed
Sabbir Hossain <sabbir2always@gmail.com>
2025-07-17 16:05:38 +0600

    fix search-params issue

--- app/(routes)/tour/page.jsx
+++ app/(routes)/tour/page.jsx
@@ -1,5 +1,10 @@
 import TourMain from "@/app/components/Map/TourMain";
+import { Suspense } from "react";
 
 export default function page() {
-  return <TourMain />;
+  return (
+    <Suspense>
+      <TourMain />
+    </Suspense>
+  );
 }
--- app/components/Dashboard/ManageTour.jsx
+++ app/components/Dashboard/ManageTour.jsx
@@ -1,5 +1,10 @@
 import TourMain from "../Map/TourMain";
+import { Suspense } from "react";
 
 export default function ManageTour() {
-  return <TourMain pageType={"admin"} />;
+  return (
+    <Suspense>
+      <TourMain pageType={"admin"} />;
+    </Suspense>
+  );
 }
